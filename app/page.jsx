b3d76b7b9c3e824f8bea06d210d8ab91fddae4c4
hienthao339 <hienthao338@gmail.com>
2024-10-29 17:21:05 +0700

    mobile nav + ongoing homepage design

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,45 @@
-export default function Home() {
+import { Button } from "@/components/ui/button";
+import { FiDownload } from "react-icons/fi";
+
+const Home = () => {
   return (
-    <>
-      <div className="text-xl mx-0">Body</div>
-    </>
+    <section className="h-full py-4">
+      <div className="container mx-auto h-full">
+        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
+          {/* About */}
+          <div className="mb-2 flex flex-col text-center xl:text-left">
+            <span className="text-xl">Frontend Developer</span>
+            <h1 className="xl:text-[56px] text-[48px] font-bold mb-4">
+              Hello I’m <br />
+              <span className="text-accent">Tran Hien Thao</span>
+            </h1>
+            <p className="text-white/80 max-w-[500px] mb-9">
+              I excel at crafting elegant product design and I am driving for
+              enhancing my proficiency in diverse technologies to lit your
+              experiences.
+            </p>
+
+            {/* Contact */}
+            <div className="flex flex-col xl:flex-row items-center gap-8 xl:mb-0 mb-10">
+              {/* Button */}
+              <div>
+                <Button variant="outline" size="lg">
+                  <span>Download CV</span>
+                  <FiDownload className="ml-2" />
+                </Button>
+              </div>
+
+              {/* Social */}
+              <div className="flex gap-4">Social</div>
+            </div>
+          </div>
+
+          {/* Avatar */}
+          <div className="">abc</div>
+        </div>
+      </div>
+    </section>
   );
-}
+};
+
+export default Home;
--- components/Header.jsx
+++ components/Header.jsx
@@ -1,19 +1,21 @@
 import Link from "next/link";
 import { Button } from "./ui/button";
 import Nav from "./Nav";
+import MobileNav from "./MobileNav";
+
 const Header = () => {
   return (
     <header className="py-8 xl:py-12 bg-zinc-900">
       <div className="container mx-auto flex items-center justify-between">
         {/* Logo */}
         <Link href="/">
-          <h1 className="text-4xl font-semibold">
+          <h1 className="h1">
             Thao<span className="text-accent">.</span>
           </h1>
         </Link>
 
         {/* Navbar - Desktop mode */}
-        <div className="hidden lg:flex gap-8 items-center">
+        <div className="hidden xl:flex gap-8 items-center">
           <Nav />
           <Link href="/contact">
             <Button>Hire me</Button>
@@ -21,8 +23,10 @@ const Header = () => {
         </div>
 
         {/* Navbar - Desktop mode */}
-        <div className="lg:hidden">
-          <nav>mobile nav</nav>
+        <div className="xl:hidden">
+          <nav>
+            <MobileNav />
+          </nav>
         </div>
       </div>
     </header>
